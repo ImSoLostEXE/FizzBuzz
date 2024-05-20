007bdcb7aca94dce4952b3862c78515b2fa12523
ImSoLostEXE <j.forline1@gmail.com>
2024-05-20 09:25:07 -0500

    Add project files.

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace FizzBuzzinator1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            Console.WriteLine("FizzBuzzinator");
+            int t = Convert.ToInt32(Console.ReadLine());
+            
+            string[] fizzability = new string[t + 1];
+
+            for (int i = 0; i <= t; i++)
+            {
+                string fizzpilation = "";
+
+                if (i % 3 == 0)
+                {
+                    fizzpilation += "Fizz";
+                }
+                if (i % 5 == 0)
+                {
+                    fizzpilation += "Buzz";
+                }
+                if (fizzpilation.Length == 0)
+                {
+                    fizzpilation += Convert.ToString(i);
+                }
+                fizzability[i] = fizzpilation;
+            }
+            Console.Write(string.Join("\n",fizzability));
+        }
+        
+    }
+
+}
